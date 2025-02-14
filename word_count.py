104c8f82a89c5e692aea2906bb1b4af2c2a0b151
teddygood <ibear6954@gmail.com>
2025-02-14 15:43:56 +0900

    test

--- word_count.py
+++ word_count.py
@@ -3,14 +3,11 @@ def word_count(file_path: str) -> dict:
     Count the frequency of each word in a file.
     """
     counts = {}
-    with open(file_path 'r') as f:
+    with open(file_path, 'r') as f:
         for line in f:
             words = line.split()
             for w in words:
-                word = word.lower()
-                if word not in counts:
-                    counts[word] = 1
-                else:
-                    counts[word] += 1
+                word = w.lower()
+                counts[word] = counts.get(word, 0) + 1
 
     return counts
